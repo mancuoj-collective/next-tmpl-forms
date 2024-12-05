b473d6a554bfbe21aed3672d31e82f3a2a4d1073
mancuoj <mancuoj@gmail.com>
2024-12-05 20:22:40 +0800

    chore: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import '@/globals.css'
 
 import type { Metadata } from 'next'
 import { Inter, Lora } from 'next/font/google'
+import Script from 'next/script'
 
+import { env } from '@/lib/env'
 import { cn } from '@/lib/utils'
 
 import { AppProvider } from './provider'
@@ -21,6 +23,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn('font-sans antialiased', inter.variable, lora.variable)}>
+        {/* TODO: remove umami if not needed */}
+        {env.NODE_ENV === 'production' && (
+          <Script
+            src="https://a.mancuoj.me/script.js"
+            data-website-id="7c1b8609-8069-4f19-999d-c08bd2c57a55"
+          />
+        )}
         <AppProvider>
           {children}
         </AppProvider>
